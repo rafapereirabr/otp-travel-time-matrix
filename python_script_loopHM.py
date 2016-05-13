ce8acac2178624f3115b8072c8efe8f9d488e512
Rafael Pereira <rafa.pereira.br@gmail.com>
2016-05-13 16:35:23 +0100

    python script

---
+++ python_script_loopHM.py
@@ -0,0 +1,55 @@
+#!/usr/bin/jython
+from org.opentripplanner.scripting.api import OtpsEntryPoint
+
+
+# Instantiate an OtpsEntryPoint
+otp = OtpsEntryPoint.fromArgs(['--graphs', '.',
+                               '--router', 'portland'])
+
+# Start timing the code
+import time
+start_time = time.time()
+
+# Get the default router
+router = otp.getRouter('portland')
+
+
+# Read Points of Destination - The file points.csv contains the columns GEOID, X and Y.
+points = otp.loadCSVPopulation('points.csv', 'Y', 'X')
+dests = otp.loadCSVPopulation('points.csv', 'Y', 'X')
+
+
+for h in range(7, 13):      # Loop every hour between 7h and 13h
+  for m in range(0,60,10):  # Loop every 10 minutes
+  
+    # Create a default request for a given time
+    req = otp.createRequest()
+    req.setDateTime(2015, 9, 15, h, m, 00)  # set departure time
+    req.setMaxTimeSec(7200)                 # set a limit to maximum travel time (seconds)
+    req.setModes('WALK,BUS,RAIL')           # define transport mode
+    
+    
+    # Create a CSV output
+    matrixCsv = otp.createCSVOutput()
+    matrixCsv.setHeader([ 'year','depart_time', 'origin', 'destination', 'walk_distance', 'travel_time' ])
+    
+    # Start Loop
+    for origin in points:
+      print "Processing origin: ", str(h)+"-"+str(m)," ", origin
+      req.setOrigin(origin)
+      spt = router.plan(req)
+      if spt is None: continue
+    
+      # Evaluate the SPT for all points
+      result = spt.eval(dests)
+      
+      # Add a new row of result in the CSV output
+      for r in result:
+        matrixCsv.addRow([ 2015, str(h) + ":" + str(m) + ":00", origin.getStringData('GEOID'), r.getIndividual().getStringData('GEOID'), r.getWalkDistance() , r.getTime()])
+    
+    # Save the result
+      matrixCsv.save('traveltime_matrix_'+ str(h)+"-"+str(m) + '.csv')
+
+
+# Stop timing the code
+print("Elapsed time was %g seconds" % (time.time() - start_time))
